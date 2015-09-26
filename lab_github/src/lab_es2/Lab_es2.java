6e289afdc173265990ef7f026e827b953af486b6
Letícia <Letcia@Letcia>
2015-09-25 21:51:46 -0300

    Versão 1.0

---
+++ lab_github/src/lab_es2/Lab_es2.java
@@ -0,0 +1,30 @@
+package lab_es2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Fellype, Juliana e Letícia
+ */
+public class Lab_es2 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Olá! Qual seu nome? ");
+        String nome = s.next();
+        System.out.println("");
+
+        System.out.println("Bem vindo(a), " + nome + "!");
+        System.out.println("E obrigada por participar do exercício de laboratório de Engenharia de Software II!");
+        System.out.println("");
+
+        System.out.println("Exercício feito pelos alunos: Fellype, Juliana e Letícia");
+        
+        System.out.println("Professor responsável: Rafael Gamez");
+        
+        System.out.println("");
+        
+    }
+
+}
